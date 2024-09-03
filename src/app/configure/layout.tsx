94c0df78120ecf3df02f6f1078e1d467433e71d7
mustafasahin0 <sahinmustafaenes4@gmail.com>
2024-09-03 16:30:39 +0300

    added

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,13 +1,14 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper"
-import Steps from "@/components/Steps"
-import { ReactNode } from "react"
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Steps from '@/components/Steps'
+import { ReactNode } from 'react'
 
-
-const Layout = ({children} : {children : ReactNode}) => {
-    return <MaxWidthWrapper className="flex-1 flex flex-col">
-        <Steps />
-        {children}
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <MaxWidthWrapper className='flex-1 flex flex-col'>
+      <Steps />
+      {children}
     </MaxWidthWrapper>
+  )
 }
 
 export default Layout
